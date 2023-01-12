64cf0c6f5820e8b529314626c4c8384a010bd99c
akif <70863898+akif-ned@users.noreply.github.com>
2023-01-12 20:36:47 +0100

    last

--- src/test/java/T01_Selenium_Locators/Efe.java
+++
@@ -1,7 +0,0 @@
-package T01_Selenium_Locators;
-
-public class Efe {
-    public static void main(String[] args) {
-        System.out.println("efe");
-    }
-}
--- src/test/java/T01_Selenium_Locators/cengizclass.java
+++
@@ -1,8 +0,0 @@
-package T01_Selenium_Locators;
-
-public class cengizclass {
-
-    public static void main(String[] args) {
-        System.out.println("Hallo");
-    }
-}
