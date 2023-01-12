da873126a0a7629fe5092637badb5700e2fca0a5
akif <70863898+akif-ned@users.noreply.github.com>
2023-01-12 01:25:15 +0100

    11.01.2023

--- src/test/java/T01_Selenium_Locators/Q4_Tagname.java
+++ src/test/java/T01_Selenium_Locators/Q4_Tagname.java
@@ -1,10 +1,16 @@
 package T01_Selenium_Locators;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Q4_Tagname {
     @Test
     public void test01(){
-
+        // we are ganna find a page that we can locate it with tag name
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://ing.nl");
     }
 }
